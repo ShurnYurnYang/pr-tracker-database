2c963f7b1388f2ec5cb9b75fd60514e8fe460c85
ShurnYurnYang <seany071305@gmail.com>
2023-08-11 16:29:20 -0400

    tables

--- database.sql
+++ database.sql
@@ -1,6 +1,20 @@
 CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     password_hash VARCHAR(100) NOT NULL,
     discord_id_hash VARCHAR(100) NOT NULL
+);
+
+CREATE TYPE pr_stats AS (
+    name VARCHAR(100),
+    bench DECIMAL(5,1),
+    squat DECIMAL(5,1),
+    deadlift DECIMAL(10,2),
+)
+
+CREATE TABLE historical (
+    entry_id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(user_id),
+    record_date DATE NOT NULL,
+    pr pr_stats 
 );
