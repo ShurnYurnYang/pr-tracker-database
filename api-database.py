c7315755e5a47966485169bdc2b362475de371de
ShurnYurnYang <seany071305@gmail.com>
2023-08-13 01:02:29 -0400

    completed 1st and 2nd api functions

--- api-database.py
+++ api-database.py
@@ -1,5 +1,6 @@
 import psycopg2
 from flask import Flask, request, jsonify
+from datetime import datetime
 
 connect_info = {
     "host": "containers-us-west-114.railway.app",
@@ -26,5 +27,49 @@ def query_execute(query, param = None):
 
     return result
 
-
 app = Flask(__name__)
+
+## API FUNCTIONS:
+## 1. Enter new user -> enters new user into 'users' table -> gives hashed password and discord-id if applicable
+## 2. Enter new PR -> enters new pr data into 'historical' table -> generate appropriate graphs using mathplotlib
+## 3. Get user info -> gets user information -> gets hashed password and discord-id
+## 4. Get latest PR entry -> gets latest PR entry for specific user -> includes graphs
+
+## 1. get user info
+@app.route('/get_user', methods=['GET'])
+def get_user():
+    username = request.args.get('user_id')
+
+    if username:
+        query = "SELECT * FROM users WHERE username = (%s);"
+        param = (username)
+        
+        try:
+            user_data = query_execute(query, param)
+        except Exception as error:
+            return jsonify({"message": f"Server error, please try again later. Error code: {error}"}), 500
+        
+        if user_data:
+            return jsonify(user_data), 200
+        else:
+            return jsonify({"message": "User not found"}), 404
+    else:
+        return jsonify({"message': 'Missing username parameter"}), 400
+    
+## 2. enter new PR info
+@app.route('/insert_pr', methods=['POST'])
+def insert_new_pr():
+    data = request.get_json()
+
+    if data:
+        query = "INSERT INTO historical (user_id, record_date, pr) VALUES (%s, %s, ROW(%s, %s, %s, %s));"
+        param = (data['user_id'], datetime.strptime(data['record_date'], "%Y-%m-%d").date(), data['pr']['name'], data['pr']['bench'], data['pr']['squat'], data['pr']['deadlift'])
+        
+        try:
+            query_execute(query, param)
+        except Exception as error:
+            return jsonify({"message": f"Server error, please try again later. Error code: {error}"}), 500
+        
+        return jsonify({"message": "Data insertion success"}), 201
+    else:
+        return jsonify({"message": "Data not found"}), 404
